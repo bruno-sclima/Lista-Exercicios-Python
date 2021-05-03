3aa21644936c21f217ef81ff07746203d3644a3e
Bruno Silveira Cequeira Lima <brunobhsclima@gmail.com>
2021-05-03 15:42:05 -0300

    Update q7.py

--- Codigo/q7.py
+++ Codigo/q7.py
@@ -2,8 +2,4 @@ n = int (input("Digite o numero de linhas: "))
 for i in range(n):
   print(' ' * (n - i - 1) + "*" * (2 * i + 1))
 
-n = int (input("Digite o numero de linhas: "))
-j = n
-while j > 0:
-    print(' '* (n - j - 1)+ "*"*(2*j +1))
-    j= j -1
+
